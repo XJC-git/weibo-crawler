4c40757890a2e094b5a05f4c3ca17f026cc4a55a
XJC-git <979033815.aa@gmail.com>
2024-05-31 19:33:07 -0400

    add sync to aws

---
+++ upload_to_database.py
@@ -0,0 +1,103 @@
+import logging
+import os
+from datetime import datetime
+
+import pandas as pd
+import shutil
+import pymysql
+
+logger = logging.getLogger("sync")
+
+
+def convert_date(date_str):
+    return datetime.strptime(date_str, '%Y%m%dT').strftime('%Y-%m-%d')
+
+
+class Sync:
+    def __init__(self, config):
+        self.mysql_config = config["mysql_config"]
+
+    def scan_img(self):
+        df = pd.read_csv("weibo/users.csv")
+        users = df['昵称'].tolist()
+        f_list = os.listdir("weibo")
+        tweets = {}
+        for user in users:
+            base_dir = "weibo/" + user
+            if not os.path.exists(base_dir):
+                continue
+            for img in os.listdir(base_dir + '/img/原创微博图片'):
+                img_path = base_dir + '/img/原创微博图片/' + img
+                file_size = os.path.getsize(img_path)
+                if file_size < 1000:
+                    continue
+                params = img.split('_')
+                date = params[0]
+                weibo_id = params[1]
+                order = params[2].split('.')[0]
+                if weibo_id not in tweets:
+                    tweets[weibo_id] = {
+                        'date': date,
+                        'author': user,
+                        'images': {
+                            order: '/static/weibo/' + img
+                        }
+                    }
+                else:
+                    tweets[weibo_id]['images'][order] = '/static/weibo/' + img
+                #shutil.move(img_path, 'static/weibo/' + img)
+        return tweets
+
+    def upload_to_database(self):
+        tweets = self.scan_img()
+        connection = pymysql.connect(**self.mysql_config)
+        cursor = connection.cursor()
+        for weibo_id in tweets:
+            tweet = tweets[weibo_id]
+            new_tweet = {
+                'date': convert_date(tweet['date']),
+                'author': tweet['author'],
+                'contains_image': len(tweet['images']),
+                'note': weibo_id,
+                "source": "weibo"
+            }
+            sql = self.mysql_insert_sql(new_tweet, "tweets")
+            tweet_id = -1
+            try:
+                cursor.execute(sql, list(new_tweet.values()))
+                tweet_id = cursor.lastrowid
+            except Exception as e:
+                connection.rollback()
+                raise e
+
+            if tweet_id == -1:
+                continue
+
+            for order in tweets[weibo_id]['images']:
+                new_img = {
+                    "path": tweets[weibo_id]['images'][order],
+                }
+                sql = self.mysql_insert_sql(new_img, "images")
+                try:
+                    cursor.execute(sql, list(new_img.values()))
+                    img_id = cursor.lastrowid
+                    new_rel = {
+                        "image_id": img_id,
+                        "tweet_id": tweet_id,
+                        "sequence": int(order),
+                    }
+                    sql = self.mysql_insert_sql(new_rel, "rel_tweets_images")
+                    cursor.execute(sql, list(new_rel.values()))
+                except Exception as e:
+                    connection.rollback()
+                    raise e
+        connection.commit()
+        connection.close()
+
+    def mysql_insert_sql(self, data, table):
+        values = ",".join(["%s"] * len(data))
+        keys = ",".join(data.keys())
+        sql = """INSERT INTO {table}({keys}) VALUES ({values})""".format(
+            table=table, keys=keys, values=values
+        )
+        return sql
